74067d171609a92116dbf3c38d70e8348119ae7d
Alex Hughes <ahughesalex@gmail.com>
2024-09-06 14:09:08 -0700

    Update to epmog's font_installer

--- src/deadline/ae_adaptor/AEAdaptor/font_installer.py
+++ src/deadline/ae_adaptor/AEAdaptor/font_installer.py
@@ -1,15 +1,13 @@
+import argparse
 import os
 import shutil
 import ctypes
 from ctypes import wintypes
-import logging
 
 try:
     import winreg
 except ImportError:
     import _winreg as winreg
-
-_logger = logging.getLogger(__name__)
 
 user32 = ctypes.WinDLL("user32", use_last_error=True)
 gdi32 = ctypes.WinDLL("gdi32", use_last_error=True)
@@ -39,11 +37,13 @@ except Exception:
         "Windows",
         "Fonts"
     )
+FONT_EXTENSIONS = {".OTF", "TTF"}
 
 # Check if the Fonts folder exists, create it if it doesn't
 if not os.path.exists(FONT_LOCATION_USER):
-    _logger.info("Creating User Fonts folder: %s" % FONT_LOCATION_USER)
+    print("Creating User Fonts folder: %s" % FONT_LOCATION_USER)
     os.makedirs(FONT_LOCATION_USER)
+
 
 def install_font(src_path, scope=INSTALL_SCOPE_USER):
     try:
@@ -59,7 +59,7 @@ def install_font(src_path, scope=INSTALL_SCOPE_USER):
         # load the font in the current session
         if not gdi32.AddFontResourceW(dst_path):
             os.remove(dst_path)
-            raise WindowsError('AddFontResource failed to load "%s"' % src_path)
+            raise Exception("AddFontResource failed to load \"%s\"" % src_path)
         # notify running programs
         user32.SendMessageTimeoutW(
             HWND_BROADCAST, WM_FONTCHANGE, 0, 0, SMTO_ABORTIFHUNG, 1000, None
@@ -122,7 +122,7 @@ def uninstall_font(src_path, scope=INSTALL_SCOPE_USER):
         # unload the font in the current session
         if not gdi32.RemoveFontResourceW(dst_path):
             os.remove(dst_path)
-            raise WindowsError('RemoveFontResourceW failed to load "%s"' % src_path)
+            raise Exception("RemoveFontResourceW failed to load \"%s\"" % src_path)
 
         if os.path.exists(dst_path):
             os.remove(dst_path)
@@ -136,3 +136,68 @@ def uninstall_font(src_path, scope=INSTALL_SCOPE_USER):
 
         return False, traceback.format_exc()
     return True, ""
+
+
+def _find_fonts(folder):
+    fonts = set()
+    for path in os.listdir(folder):
+        print("path: " + path)
+        if path.startswith("assetroot-"):
+            asset_dir = os.path.join(folder, path)
+            print("  asset_dir: " + asset_dir)
+            for asset_path in os.listdir(asset_dir):
+                print("    asset_path: " + asset_path)
+                _, ext = os.path.splitext(os.path.join(asset_dir, asset_path))
+                if ext.upper() in FONT_EXTENSIONS:
+                    fonts.add(os.path.join(asset_dir, asset_path))
+    return fonts
+
+
+def _install_fonts(folder):
+    fonts = _find_fonts(folder)
+    installed_fonts = set()
+    if not fonts:
+        print("No fonts to install")
+        return
+
+    for font in fonts:
+        print("Installing font: %s" % font)
+        installed, msg = install_font(font)
+        if not installed:
+            print("    Error installing font: %s" % msg)
+        else:
+            installed_fonts.add(font)
+    return installed_fonts
+
+
+def _remove_fonts(folder):
+    fonts = _find_fonts(folder)
+    if not fonts:
+        print("No fonts to uninstall")
+        return
+
+    for font in fonts:
+        print("Uninstalling font: %s" % font)
+        uninstalled, msg = uninstall_font(font)
+        if not uninstalled:
+            print("    Error uninstalling font: %s" % msg)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="InstallFonts",
+        description="Installs and uninstalls fonts for After Effects")
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-i", "--install", metavar="FOLDER_PATH", help="Install font located in FOLDER_PATH")
+    group.add_argument("-un", "--uninstall", metavar="FOLDER_PATH", help="Remove fonts located in FOLDER_PATH")
+
+    args = parser.parse_args()
+    if args.install:
+        _install_fonts(args.install)
+    elif args.uninstall:
+        _remove_fonts(args.uninstall)
+    else:
+        raise RuntimeError("Argparse forces you to specify install or uninstall")
+
+    print("Done running font installer")
